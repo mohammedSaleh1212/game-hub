54cc7c61adcec148e965f081c040ae10987f03da
mohammedSaleh1212 <mahtrbtktk@gmail.com>
2024-03-18 23:54:29 +0300

    install chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
 
   return (
     <>
+
+      <Button colorScheme='blue'>Button</Button>
 
     </>
   )
